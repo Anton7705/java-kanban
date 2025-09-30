ecc4c0cfc2d8ee9ac60fa77c26e1bffdaeaa8f7d
AntonDor <dorogovanton291@gmail.com>
2025-09-30 23:28:08 +0300

    feat (Subtask.java): подзадача с тем же id что и у Эпика не может быть добавлена к Эпику

--- src/ru/yandex/javacourse/schedule/tasks/Subtask.java
+++ src/ru/yandex/javacourse/schedule/tasks/Subtask.java
@@ -3,13 +3,11 @@ package ru.yandex.javacourse.schedule.tasks;
 public class Subtask extends Task {
 	protected int epicId;
 
-	public Subtask(int id, String name, String description, TaskStatus status, int epicId) {
-		super(id, name, description, status);
-		this.epicId = epicId;
-	}
-
 	public Subtask(String name, String description, TaskStatus status, int epicId) {
 		super(name, description, status);
+		if (this.id == epicId) {
+			throw new IllegalArgumentException("Subtask cannot be attached to itself");
+		}
 		this.epicId = epicId;
 	}
 
@@ -28,3 +26,4 @@ public class Subtask extends Task {
 				'}';
 	}
 }
+
