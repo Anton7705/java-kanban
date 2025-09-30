4e4ecc4973e089023f3fdbddaabf9657e112aac6
AntonDor <dorogovanton291@gmail.com>
2025-09-30 23:28:20 +0300

    feat (Main.java): Выполнение доп задания по написанию отладочных принтов в main

--- src/ru/yandex/javacourse/schedule/Main.java
+++ src/ru/yandex/javacourse/schedule/Main.java
@@ -28,84 +28,43 @@ public class Main {
 
 		Subtask subtask1 = new Subtask("Subtask #1-1", "Subtask1 description", NEW, epicId1);
 		Subtask subtask2 = new Subtask("Subtask #2-1", "Subtask1 description", NEW, epicId1);
-		Subtask subtask3 = new Subtask("Subtask #3-2", "Subtask1 description", DONE, epicId2);
+		Subtask subtask3 = new Subtask("Subtask #3-2", "Subtask1 description", DONE, epicId1);
 		manager.addNewSubtask(subtask1);
-		final Integer subtaskId2 = manager.addNewSubtask(subtask2);
-		final Integer subtaskId3 = manager.addNewSubtask(subtask3);
+		manager.addNewSubtask(subtask2);
+		manager.addNewSubtask(subtask3);
 
-		printAllTasks(manager);
+		manager.getEpic(epicId1);
+		manager.getSubtask(subtask3.getId());
+		manager.getEpic(epicId2);
+		manager.getSubtask(subtask2.getId());
+		manager.getSubtask(subtask1.getId());
+		manager.getTask(task1.getId());
 
-		// Обновление
-		final Task task = manager.getTask(taskId2);
-		task.setStatus(DONE);
-		manager.updateTask(task);
-		System.out.println("CHANGE STATUS: Task2 IN_PROGRESS->DONE");
-		System.out.println("Задачи:");
-		for (Task t : manager.getTasks()) {
-			System.out.println(t);
-		}
-
-		Subtask subtask = manager.getSubtask(subtaskId2);
-		subtask.setStatus(DONE);
-		manager.updateSubtask(subtask);
-		System.out.println("CHANGE STATUS: Subtask2 NEW->DONE");
-		subtask = manager.getSubtask(subtaskId3);
-		subtask.setStatus(NEW);
-		manager.updateSubtask(subtask);
-		System.out.println("CHANGE STATUS: Subtask3 DONE->NEW");
-		System.out.println("Подзадачи:");
-		for (Task t : manager.getSubtasks()) {
-			System.out.println(t);
-		}
-
-		System.out.println("Эпики:");
-		for (Task e : manager.getEpics()) {
-			System.out.println(e);
-			for (Task t : manager.getEpicSubtasks(e.getId())) {
-				System.out.println("--> " + t);
-			}
-		}
-		final Epic epic = manager.getEpic(epicId1);
-		epic.setStatus(NEW);
-		manager.updateEpic(epic);
-		System.out.println("CHANGE STATUS: Epic1 IN_PROGRESS->NEW");
-		printAllTasks(manager);
-
-		System.out.println("Эпики:");
-		for (Task e : manager.getEpics()) {
-			System.out.println(e);
-			for (Task t : manager.getEpicSubtasks(e.getId())) {
-				System.out.println("--> " + t);
-			}
-		}
-
-		// Удаление
-		System.out.println("DELETE: Task1");
-		manager.deleteTask(taskId1);
-		System.out.println("DELETE: Epic1");
-		manager.deleteEpic(epicId1);
-		printAllTasks(manager);
-	}
-
-	private static void printAllTasks(TaskManager manager) {
-		System.out.println("Задачи:");
-		for (Task task : manager.getTasks()) {
+		System.out.println("История 1 :");
+		for (Task task : manager.getHistory()) {
 			System.out.println(task);
 		}
-		System.out.println("Эпики:");
-		for (Task epic : manager.getEpics()) {
-			System.out.println(epic);
-//			System.out.println("--> Подзадачи эпика:");
-			for (Task task : manager.getEpicSubtasks(epic.getId())) {
-				System.out.println("--> " + task);
-			}
-		}
-		System.out.println("Подзадачи:");
-		for (Task subtask : manager.getSubtasks()) {
-			System.out.println(subtask);
+
+		manager.getTask(task1.getId());
+		manager.getSubtask(subtask2.getId());
+		manager.getSubtask(subtask3.getId());
+
+		System.out.println("История 2 :");
+		for (Task task : manager.getHistory()) {
+			System.out.println(task);
 		}
 
-		System.out.println("История:");
+		manager.deleteTask(taskId1);
+		manager.deleteEpic(epicId2);
+
+		System.out.println("История 3 :");
+		for (Task task : manager.getHistory()) {
+			System.out.println(task);
+		}
+
+		manager.deleteEpic(epicId1);
+
+		System.out.println("История 4 :");
 		for (Task task : manager.getHistory()) {
 			System.out.println(task);
 		}
