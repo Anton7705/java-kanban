e1be70a17c49e555557431872fe8ab31bfed5e54
AntonDor <dorogovanton291@gmail.com>
2025-10-20 00:40:41 +0300

    fix: codeStyle

--- src/ru/yandex/javacourse/schedule/Main.java
+++ src/ru/yandex/javacourse/schedule/Main.java
@@ -15,6 +15,7 @@ import java.io.File;
 import java.io.IOException;
 
 public class Main {
+
 	public static void main(String[] args) {
 
 //		Дополнительное задание. Реализуем пользовательский сценарий
